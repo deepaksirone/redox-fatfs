19dc5ca160c8e788fc200f24f3930c8397842d38
Deepak Sirone <deepaksirone94@gmail.com>
2019-02-26 00:10:27 +0530

    Add gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
-
+struct BiosParameterBlock {
+    // Jump instructions to boot code
+    boot_jmp: [u8; 3],
+    oem_name: [u8; 8],
+    // Size of a sector, actual size: 2 bytes
+    sector_size: u32,
+    
+}
+struct File {
+    pos: u64,
+}
