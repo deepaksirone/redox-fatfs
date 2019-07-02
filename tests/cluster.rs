b8fbe93853b2655e0da1afe258d11a694fc51f6a
Deepak Sirone <deepaksirone94@gmail.com>
2019-07-02 14:46:19 +0530

    Update Tests

--- tests/cluster.rs
+++ tests/cluster.rs
@@ -11,13 +11,18 @@ use redox_fatfs::*;
 fn print_bpb() {
     let mut f = fs::File::open("images/fat32.img").unwrap();
     let mut fs = redox_fatfs::FileSystem::from_offset(0, f).expect("Parsing Error");
-    let root_clus = Cluster::new(2);
+    let root_clus = Cluster::new(10);
     println!("Root Cluster = {:?}", fs.clusters(root_clus));
     let mut buf = [0; 32];
     fs.read_cluster(root_clus, &mut buf);
     println!("Buffer = {:?}", buf);
     println!("BPB = {:?}", fs.bpb);
     println!("FsInfo = {:?}", fs.fs_info.borrow());
+    println!("Mirroring Enabled = {:?}", fs.mirroring_enabled());
+
+    let free = get_free_cluster(&mut fs, Cluster::new(11), Cluster::new(100));
+    println!("Free Cluster = {:?}", free);
+
 
 }
 
